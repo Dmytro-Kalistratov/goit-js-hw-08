6bbf341fe9b250c88fc9b05b38b9e06ecd21cfaa
Dmytro Kalistratov <wobupeld@gmail.com>
2021-11-23 22:51:15 +0200

    Quest #2
    lodash - not cool

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,16 @@
+const throttle = require("lodash.throttle");
 
+const iframe = document.querySelector('iframe');
+const player = new Vimeo.Player(iframe);
+
+if (localStorage.getItem("videoplayer-current-time") !== null) {
+    player.setCurrentTime(localStorage.getItem("videoplayer-current-time"));
+};
+
+const saveCurrentTime = function(data) {
+    
+    localStorage.setItem("videoplayer-current-time", data.seconds);
+
+};
+
+player.on('timeupdate',  throttle(saveCurrentTime, 1000));
