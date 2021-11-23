1d7e5754203fe367386bdb49e6d8d6b94a94dc40
Dmytro Kalistratov <wobupeld@gmail.com>
2021-11-23 21:34:25 +0200

    Quest #1 done

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,31 @@
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+
+let galleryMarkup = '';
+
+galleryItems.forEach((galleryItem) => {
+
+    const itemMarkup =
+    `<a class="gallery__link" href="${galleryItem.original}">
+        <img
+        class="gallery__image"
+        src="${galleryItem.preview}"
+        alt="${galleryItem.description}"
+        />
+    </a>`
+
+    galleryMarkup += itemMarkup;
+});
+
+const galleryElem = document.querySelector('.gallery');
+galleryElem.insertAdjacentHTML('beforeend', galleryMarkup);
+
+const galleryLightbox = new SimpleLightbox('.gallery a', { captionsData: "alt", captionDelay: 250, });
+
+
 
 console.log(galleryItems);
